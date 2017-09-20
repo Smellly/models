02abe5c4645e6bfc0aa7d3bc5796a85aa289bcb8
Jay <schenxmu@gmail.com>
2017-09-20 15:52:22 +0800

    add adaboost in mm

---
+++ im2txt-ensemble/adaboost/inference_utils/mm_caption_generator.py
@@ -0,0 +1,232 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Class for generating captions from an image-to-text model."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import heapq
+import math
+
+import numpy as np
+
+'''
+initial_beam = Caption(
+    sentence=[self.vocab.start_id],
+    state=initial_state[0],
+    logprob=0.0,
+    score=0.0,
+    metadata=[""])
+'''
+class Caption(object):
+  """Represents a complete or partial caption."""
+
+  def __init__(self, sentence, state, logprob, score, metadata=None):
+    """Initializes the Caption.
+
+    Args:
+      sentence: List of word ids in the caption.
+      state: Model state after generating the previous word.
+      logprob: Log-probability of the caption.
+      score: Score of the caption.
+      metadata: Optional metadata associated with the partial sentence. If not
+        None, a list of strings with the same length as 'sentence'.
+    """
+    self.sentence = sentence
+    self.state = state
+    self.logprob = logprob
+    self.score = score
+    self.metadata = metadata
+
+  def __cmp__(self, other):
+    """Compares Captions by score."""
+    assert isinstance(other, Caption)
+    if self.score == other.score:
+      return 0
+    elif self.score < other.score:
+      return -1
+    else:
+      return 1
+  
+  # For Python 3 compatibility (__cmp__ is deprecated).
+  def __lt__(self, other):
+    assert isinstance(other, Caption)
+    return self.score < other.score
+  
+  # Also for Python 3 compatibility.
+  def __eq__(self, other):
+    assert isinstance(other, Caption)
+    return self.score == other.score
+
+
+class TopN(object):
+  """Maintains the top n elements of an incrementally provided set."""
+
+  def __init__(self, n):
+    self._n = n
+    self._data = []
+
+  def size(self):
+    assert self._data is not None
+    return len(self._data)
+
+  def push(self, x):
+    """Pushes a new element."""
+    assert self._data is not None
+    if len(self._data) < self._n:
+      # Push the value item onto the heap, maintaining the heap invariant.
+      heapq.heappush(self._data, x)
+    else:
+      # Pop and return the smallest item from the heap, maintaining the heap invariant. 
+      heapq.heappushpop(self._data, x)
+
+  def extract(self, sort=False):
+    """Extracts all elements from the TopN. This is a destructive operation.
+
+    The only method that can be called immediately after extract() is reset().
+
+    Args:
+      sort: Whether to return the elements in descending sorted order.
+
+    Returns:
+      A list of data; the top n elements provided to the set.
+    """
+    assert self._data is not None
+    data = self._data
+    self._data = None
+    if sort:
+      data.sort(reverse=True)
+    return data
+
+  def reset(self):
+    """Returns the TopN to an empty state."""
+    self._data = []
+
+
+class CaptionGenerator(object):
+  """Class to generate captions from an image-to-text model."""
+
+  def __init__(self,
+               models,
+               vocab,
+               beam_size=3,
+               max_caption_length=20,
+               length_normalization_factor=0.0):
+    """Initializes the generator.
+
+    Args:
+      models: multiple models for ensemble. 
+        Object encapsulating a trained image-to-text model. Must have
+        methods feed_image() and inference_step(). For example, an instance of
+        InferenceWrapperBase.
+      vocab: A Vocabulary object.
+      beam_size: Beam size to use when generating captions.
+      max_caption_length: The maximum caption length before stopping the search.
+      length_normalization_factor: If != 0, a number x such that captions are
+        scored by logprob/length^x, rather than logprob. This changes the
+        relative scores of captions depending on their lengths. For example, if
+        x > 0 then longer captions will be favored.
+    """
+    self.vocab = vocab
+    self.models = models
+    self.num_models = len(models)
+
+    self.beam_size = beam_size
+    self.max_caption_length = max_caption_length
+    self.length_normalization_factor = length_normalization_factor
+
+  def beam_search(self, encoded_image):
+    """Runs beam search caption generation on a single image.
+
+    Args:
+      sesses: multiple TensorFlow Session objects.
+      encoded_image: An encoded image string.
+
+    Returns:
+      A list of Caption sorted by descending score.
+    """
+    # Feed in the image to get the initial state.
+    partial_captions = TopN(self.beam_size)
+    complete_captions = TopN(self.beam_size)
+
+    for ind, m in enumerate(self.models):
+      initial_state = m['model'].feed_image(m['sess'], encoded_image)
+
+      initial_beam = Caption(
+          sentence=[self.vocab.start_id],
+          state=initial_state[0],
+          logprob=0.0,
+          score=0.0,
+          metadata=[""])
+    
+      partial_captions.push(initial_beam)
+      # self.models[ind]['partial_captions'] = (partial_captions,)
+
+    # Run beam search.
+    for _ in range(self.max_caption_length - 1):
+      p0 = 0.
+      partial_captions_list = partial_captions.extract()
+      input_feed = np.array([c.sentence[-1] for c in partial_captions_list])
+      state_feed = np.array([c.state for c in partial_captions_list])
+
+      for ind, m in enumerate(self.models):
+        # partial_captions = m['partial_captions']
+        # partial_captions_list.extend(partial_captions.extract())
+        softmax, new_states, metadata = m['model'].inference_step(m['sess'],
+                                                                  input_feed,
+                                                                  state_feed)
+        # self.models[ind]['state'] = new_states
+        # self.models[ind]['metadata'] = metadata # None actually
+
+      partial_captions.reset()
+      p0 += softmax
+      p0 /= self.num_models
+
+      for i, partial_caption in enumerate(partial_captions_list):
+        word_probabilities = p0[i]
+        state = new_states[i]
+        # For this partial caption, get the beam_size most probable next words.
+        words_and_probs = list(enumerate(word_probabilities))
+        words_and_probs.sort(key=lambda x: -x[1])
+        words_and_probs = words_and_probs[0:self.beam_size]
+        # Each next word gives a new partial caption.
+        for w, p in words_and_probs:
+          if p < 1e-12:
+            continue  # Avoid log(0).
+          sentence = partial_caption.sentence + [w]
+          logprob = partial_caption.logprob + math.log(p)
+          score = logprob
+
+          if w == self.vocab.end_id:
+            if self.length_normalization_factor > 0:
+              score /= len(sentence)**self.length_normalization_factor
+            beam = Caption(sentence, state, logprob, score, None)
+            complete_captions.push(beam)
+          else:
+            beam = Caption(sentence, state, logprob, score, None)
+            partial_captions.push(beam)
+
+      if partial_captions.size() == 0:
+        # We have run out of partial candidates; happens when beam_size = 1.
+        break
+
+    # If we have no complete captions then fall back to the partial captions.
+    # But never output a mixture of complete and partial captions because a
+    # partial caption could have a higher score than all the complete captions.
+    if not complete_captions.size():
+      complete_captions = partial_captions
+
+    return complete_captions.extract(sort=True)
---
+++ im2txt-ensemble/adaboost/mm_val.py
@@ -0,0 +1,191 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+r"""Generate captions for images using default beam search parameters."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import os
+import numpy as np
+import json
+
+from ast import literal_eval
+from tqdm import tqdm
+
+import tensorflow as tf
+
+from ensemble import configuration
+from ensemble import inference_wrapper
+from ensemble.inference_utils import mm_caption_generator as caption_generator
+from ensemble.inference_utils import vocabulary
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_string("checkpoint_path", "",
+                       "Model checkpoint file or directory containing a "
+                       "model checkpoint file.")
+tf.flags.DEFINE_string("vocab_file", "", "Text file containing the vocabulary.")
+tf.flags.DEFINE_string("input_files", "",
+                       "File pattern or comma-separated list of file patterns "
+                       "of image files.")
+tf.flags.DEFINE_string("output_files", "",
+                       "validation set saving path.")
+tf.flags.DEFINE_string("debug_mode", "",
+                       "debug or not.")
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def getValID(path):
+  with open(path, 'r') as f:
+    raw = json.load(f)
+  return raw['images']
+
+def getValAttr(path):
+  with open(path, 'r') as f:
+    attr_data = json.load(f)
+  filename_to_attribute = {}
+  for filename, attribute in attr_data.iteritems():
+    p = [literal_eval(i.split(':')[1])[0] for i in attribute]
+    filename_to_attribute[filename] = p 
+  return filename_to_attribute
+
+def main(_):
+  debug_mode = True if FLAGS.debug_mode == 'debug' else False
+  ensemble = [2, 4, 6, 7, 8]
+  num = len(ensemble)
+  models = []
+  generators = []
+  for en in ensemble:
+    saver_def_file = FLAGS.checkpoint_path + str(en)
+    if debug_mode:
+      print('DEBUG:ensemble model:', saver_def_file)
+    # Build the inference graph.
+    g = tf.Graph()
+    tf.reset_default_graph()
+    d = {}
+
+    with g.as_default():
+      d['model'] = inference_wrapper.InferenceWrapper()
+      model_ckpt = tf.train.get_checkpoint_state(saver_def_file)
+      assert(model_ckpt != None)
+      if debug_mode:
+        print('DEBUG:', model_ckpt)
+      d['restore_fn'] = d['model'].build_graph_from_config(
+                                                configuration.ModelConfig(),
+                                                model_ckpt.model_checkpoint_path)
+      # Sessions created in this scope will run operations from `g`.
+      d['sess'] = tf.Session()
+    g.finalize()
+    d['g'] = g
+    models.append(d)
+
+  # Create the vocabulary.
+  vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
+  tf.logging.info("Generating Beam Search Model")
+  for ind, m in enumerate(models):
+    if debug_mode:
+      print('DEBUG:loading model %d:'%(ind+1))
+    m['restore_fn'](m['sess'])
+  generator = caption_generator.CaptionGenerator(models, vocab)
+
+  if debug_mode:
+    filenames = FLAGS.input_files
+  else:
+    # wait for full version of val
+    img_path, annos_path, attrs_path = FLAGS.input_files.split(",")
+    save_path = FLAGS.output_files
+
+    filenames = getValID(annos_path)
+    # filename_to_attribute = getValAttr(attrs_path)
+
+  epoch = 0
+  savefreq = 200
+  filenames = []
+  results = []
+  records = []
+  record_path = save_path.replace('results', 'records')
+  
+  tf.logging.info("record_path : %s"%record_path)
+  tf.logging.info("save_path : %s"%save_path)
+  try:
+    tf.logging.info("Loading records")
+    with open(record_path, 'r') as f:
+      record = json.load(f)
+  except:
+    tf.logging.info("no records to read")
+
+  if debug_mode:
+    print('DEBUG:', FLAGS.input_files.split(","))
+    print('DEBUG:', FLAGS.input_files.split(",")[0])
+    print('DEBUG:', tf.gfile.Glob(FLAGS.input_files.split(",")[0]))
+
+  if debug_mode:
+    for file_pattern in FLAGS.input_files.split(","):
+      filenames.extend(tf.gfile.Glob(file_pattern))
+    tf.logging.info("Running caption generation on %d files matching %s",
+                    len(filenames), FLAGS.input_files)
+  else:
+    tf.logging.info("Loading filenames")
+    filenames = getValID(annos_path)
+
+  
+
+  for item in tqdm(filenames):
+    if debug_mode:
+      print('DEBUG:', item)
+    if debug_mode:
+      filename = item
+      print('DEBUG:', filename)
+    elif item['id'] in records:
+      continue
+    else:
+      filename = img_path + item['file_name']
+    
+    with tf.gfile.GFile(filename, "r") as f:
+      image = f.read()
+    try:
+      captions = generator.beam_search(image)
+      ppl = [math.exp(x.logprob) for x in captions]
+      caption = captions[ppl.index(max(ppl))]
+      sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+      sentence = " ".join(sentence)
+      if debug_mode:
+        print('sentence:', sentence)
+      else:
+        results.append({"image_id":item['id'], "caption":sentence})
+        records.append(item['id'])
+    except:
+      print('filename %s is broken'%item['file_name'])
+    finally:
+      if not debug_mode:
+        if epoch % savefreq == 0:
+            tf.logging.info('%d times temporally saving...'%(int(epoch/savefreq)))
+            with open(save_path, 'w') as f:
+              json.dump(results, f)
+            with open(record_path, 'w') as f:
+              json.dump(records, f)
+        epoch += 1
+
+  if not debug_mode:
+    with open(save_path, 'w') as f:
+      json.dump(results, f)
+    with open(record_path, 'w') as f:
+      json.dump(records, f)
+
+
+if __name__ == "__main__":
+  tf.app.run()
